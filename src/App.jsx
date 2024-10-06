370292760329306c998ab0ca157274632fce9dc2
ryanyunus <ryandinus@gmail.com>
2024-10-06 10:44:50 +0700

    reactdasar

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+class Button extends React.Component {
+  render() {
+    return (
+      <button
+        className="h-10 px-6 font-semibold rounded-md bg-black text-white"
+        type="submit"
+      >
+        Beli Sekarang
+      </button>
+    );
+  }
+}
+
+function App() {
+  return (
+    <div className="flex justify-center bg-blue-600 min-h-screen items-center">
+      <div>
+        <Button></Button>
+        <Button></Button>
+        <Button></Button>
+        <Button></Button>
+        <Button></Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
