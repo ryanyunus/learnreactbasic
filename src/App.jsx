b2a35ffd1f91bb3abb269f3e99de339c20dc766a
ryanyunus <ryandinus@gmail.com>
2024-10-06 11:38:05 +0700

    update3

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,24 @@
 import React from "react";
-
-class Button extends React.Component {
-  render() {
-    return (
-      <button
-        className="h-10 px-6 font-semibold rounded-md bg-red-700 text-white"
-        type="submit"
-      >
-        Beli Sekarang
-      </button>
-    );
-  }
-}
-function ButtonBlack() {
+const Button = (props) => {
+  const { children = "default", variant = "bg-black" } = props;
   return (
     <button
-      className="h-10 px-6 font-semibold rounded-md bg-black text-white"
+      className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`}
       type="submit"
     >
-      Beli Sekarang
+      {children}
     </button>
   );
-}
-function ButtonPink() {
-  return (
-    <button
-      className="h-10 px-6 font-semibold rounded-md bg-pink-700 text-white"
-      type="submit"
-    >
-      Beli Sekarang
-    </button>
-  );
-}
+};
 
 function App() {
   return (
     <div className="flex justify-center bg-blue-600 min-h-screen items-center">
-      <div>
-        <Button></Button>
-        <Button></Button>
-        <ButtonPink></ButtonPink>
-        <Button></Button>
-        <ButtonBlack></ButtonBlack>
+      <div className="flex gap-x-3">
+        <Button variant="bg-blue-700">Login</Button>
+        <Button variant="bg-slate-700">Logout</Button>
+        <Button>Guruh Fajar Ashari</Button>
+        <Button variant="bg-pink-700">anif black pink</Button>
       </div>
     </div>
   );
